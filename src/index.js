5bbcc68fa90b3df83148b6c41f56de40e9d0305e
Gatuk S Chattanon <gatukgl@gmail.com>
2017-11-18 23:58:43 +0700

    Remove semantic js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import Routes from './routes'
 
+import './styles/semantic.min.css'
+
 import './index.css';
 import App from './App';
 
