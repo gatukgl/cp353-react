eff09dd5d9c75cff7d9660901b34043baf87c260
Gatuk S Chattanon <gatukgl@gmail.com>
2017-12-15 21:22:15 +0700

    login with facebook

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import localStorage from 'localStorage'
+import FacebookLogin from 'react-facebook-login'
 
 import { login } from '../api'
 
@@ -17,58 +18,73 @@ class Login extends Component {
 
   onSubmit = event => {
     event.preventDefault()
-    login(this.state.username, this.state.password)
-      .then(data => {
-        if (data.status === 200) {
-          localStorage.setItem('username', this.state.username)       
-          this.props.history.replace('/main')
-        }
-      })
+    login(this.state.username, this.state.password).then(data => {
+      if (data.status === 200) {
+        localStorage.setItem('username', this.state.username)
+        this.props.history.replace('/main')
+      }
+    })
+  }
+
+  componentClicked = event => {
+    console.log('component click', event.target.value)
+  }
+
+  responseFacebook = result => {
+    console.log('responseFacebook', result)
   }
 
   render() {
     return (
-      <div 
-        className='ui middle aligned center aligned grid' 
+      <div
+        className="ui middle aligned center aligned grid"
         style={{ padding: '50px 0 0 50px', width: '500px' }}>
-        <div className='column'>
-          <h2 className='ui teal header'>Login</h2>
-          <form className='ui large form' onSubmit={this.onSubmit}>
-            <div className='ui stacked segment'>
-            
-              <div className='field'>
-                <div className='ui left icon input'>
-                  <i className='user icon' />
-                  <input 
-                    type='text' 
-                    name='username' 
-                    placeholder='Username' 
-                    value={this.state.username} 
-                    onChange={this.onTextChange} />
+        <div className="column">
+          <h2 className="ui teal header">Login</h2>
+          <form className="ui large form" onSubmit={this.onSubmit}>
+            <div className="ui stacked segment">
+              <div className="field">
+                <div className="ui left icon input">
+                  <i className="user icon" />
+                  <input
+                    type="text"
+                    name="username"
+                    placeholder="Username"
+                    value={this.state.username}
+                    onChange={this.onTextChange}
+                  />
                 </div>
               </div>
 
-              <div className='field'>
-                <div className='ui left icon input'>
-                  <i className='lock icon' />
-                  <input 
-                    type='password' 
-                    name='password' 
-                    placeholder='Password' 
-                    value={this.state.password} 
-                    onChange={this.onTextChange} />
+              <div className="field">
+                <div className="ui left icon input">
+                  <i className="lock icon" />
+                  <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    value={this.state.password}
+                    onChange={this.onTextChange}
+                  />
                 </div>
               </div>
 
-              <div className='field'>
-                <div className='ui left icon input'>
-                  <button type='submit' className='ui teal fluid button'>
+              <div className="field">
+                <div className="ui left icon input">
+                  <button type="submit" className="ui teal fluid button">
                     Log in
                   </button>
                 </div>
               </div>
             </div>
           </form>
+          <br/> <FacebookLogin
+            appId="774982429368366"
+            autoLoad={true}
+            fields="name,email,picture"
+            onClick={this.componentClicked}
+            callback={this.responseFacebook}
+          />
         </div>
       </div>
     )
